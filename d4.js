611318b1449be1802d7fd21c6d004e25793d7be3
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-11-07 15:27:11 -0300

    refactor: Exercise 4

--- d4.js
+++ d4.js
@@ -1,12 +1,16 @@
-function toObject(array) {
-  const obj = {};
-  array.forEach((item) => {
-    obj[item[0]] = item[1];
-  });
-  console.log(obj);
-}
-
-toObject([
+const keyAndValueArray = [
   ["c", 2],
   ["d", 4],
-]);
+];
+
+function arrayToObject(array) {
+  const obj = {};
+  array.forEach(([key, value]) => (obj[key] = value));
+
+  return obj;
+}
+console.log(arrayToObject(keyAndValueArray));
+
+// jeito 2
+
+// console.log(Object.fromEntries(keyAndValueArray));
