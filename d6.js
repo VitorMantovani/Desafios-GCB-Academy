d807e311cd6be1679efa4b40761b65293ce5a5bc
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-11-07 17:41:19 -0300

    refactor: Exercise 6

--- d6.js
+++ d6.js
@@ -1,11 +1,19 @@
-function duplicated(array) {
-  const filtred = new Array();
-  array.forEach((num) => {
-    if (!filtred.includes(num)) filtred.push(num);
-  });
-  return filtred;
+const duplicatedValuesArray = [1, 2, 3, 3, 2, 4, 5, 4, 7, 3];
+
+function removeDuplicatedValuesFromArray(array) {
+  const arrayWithoutDuplicatedValues = new Array();
+  array.forEach(
+    (element) =>
+      !arrayWithoutDuplicatedValues.includes(element) &&
+      arrayWithoutDuplicatedValues.push(element)
+  );
+  return arrayWithoutDuplicatedValues;
 }
+const response = removeDuplicatedValuesFromArray(duplicatedValuesArray);
 
-const response = duplicated([1, 2, 3, 3, 2, 4, 5, 4, 7, 3]);
+// console.log(response);
 
-console.log(response);
+// Jeito 2
+
+//Spred Operator
+console.log([...new Set(duplicatedValuesArray)]);
