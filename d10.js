bae04ab7913b4d9ad4f6aaf6fe8ceeb8e215d667
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-11-07 22:02:03 -0300

    refactor: Exercise 10

--- d10.js
+++ d10.js
@@ -1,10 +1,17 @@
-function commom(arrOne, arrTwo) {
-  const equal = [];
-  arrOne.forEach((num) => {
-    if (arrTwo.includes(num)) equal.push(num);
-  });
-  return equal;
+const arrayOne = [6, 8];
+const arrayTwo = [8, 9];
+
+function findCommomValuesBetweenArrays(arrOne, arrTwo) {
+  const commomValue = new Array();
+  arrOne.forEach(
+    (element) => arrTwo.includes(element) && commomValue.push(element)
+  );
+  return commomValue;
 }
 
-const response = commom([6, 8], [8, 9]);
+const response = findCommomValuesBetweenArrays([6, 8], [8, 9]);
 console.log(response);
+
+// Jeito 2
+
+console.log(arrayOne.filter((element) => arrayTwo.includes(element)));
